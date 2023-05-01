75b470324aa3f804341cf1d3015dccb50e29144d
Anno Langen <anno.langen@gmail.com>
2023-04-30 20:23:11 -0700

    missed file

--- step-2/include/syringe.h
+++ step-2/include/syringe.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <functional>
 
 // Wrapper class for factory function parameters to accecpt a value associated
@@ -26,9 +25,9 @@ T _provide(_Binding<T, K> &provider, I &) {
 }
 
 // A provider that returns a value created by a function
-template <class C, class K = _DefaultKey>
+template <class S, class K = _DefaultKey>
 struct _Factory {
-  C callable;
+  std::function<S> fn;
 };
 
 // Standard C++ requires a wrapper, because only classes, not
@@ -42,9 +41,9 @@ struct _InjectorWrapper<Keyed<T, K>, I> {
   static Keyed<T, K> get(I &injector) { return injector.template get<T, K>(); }
 };
 
-template <class T, class K, class I, class... A>
-T _provide(_Factory<std::function<T(A...)>, K> &f, I &injector) {
-  return std::invoke(f.callable, _InjectorWrapper<A, I>::get(injector)...);
+template <class T, class K, class... A, class I>
+T _provide(_Factory<T(A...), K> &f, I &injector) {
+  return (f.fn)(_InjectorWrapper<A, I>::get(injector)...);
 }
 
 // _BindingType<T, K, P...>::type is the _Binding<T, K> or _Factory
@@ -58,8 +57,8 @@ struct _BindingType<T, K, _Binding<T, K>, P...> {
 };
 
 template <class T, class K, class... A, class... P>
-struct _BindingType<T, K, _Factory<std::function<T(A...)>, K>, P...> {
-  using type = _Factory<std::function<T(A...)>, K>;
+struct _BindingType<T, K, _Factory<T(A...), K>, P...> {
+  using type = _Factory<T(A...), K>;
 };
 
 template <class T, class K, class H, class... P>
@@ -69,13 +68,18 @@ struct _BindingType<T, K, H, P...> {
 
 // Injector that provides values using a given set of providers.
 template <class... P>
-struct _Injector : public P... {
-  _Injector(std::tuple<P...> &providers) : P(std::get<P>(providers))... {}
+class _Injector {
+ public:
+  _Injector(std::tuple<P...> &providers) : providers_(providers) {}
 
   template <class T, class K = _DefaultKey>
   T get() {
-    return _provide((typename _BindingType<T, K, P...>::type &)*this, *this);
+    return _provide(
+        std::get<typename _BindingType<T, K, P...>::type>(providers_), *this);
   }
+
+ private:
+  std::tuple<P...> providers_;
 };
 
 // Builder for an injector.
@@ -88,16 +92,16 @@ struct _InjectorBuilder {
 
   template <class T>
   _InjectorBuilder<_Binding<T, _DefaultKey>, P...> bind(T value) {
-    return {std::make_tuple(_Binding<T, _DefaultKey>{value},
-                            std::get<P>(providers)...)};
+    return bind<_DefaultKey, T>(value);
   }
 
   template <class T, class K = _DefaultKey, class... A>
-  _InjectorBuilder<_Factory<std::function<T(A...)>, K>, P...> addFactory(
+  _InjectorBuilder<_Factory<T(A...), K>, P...> addFactory(
       std::function<T(A...)> c) {
-    return {std::make_tuple(_Factory<std::function<T(A...)>, K>{c},
-                            std::get<P>(providers)...)};
+    return {
+        std::make_tuple(_Factory<T(A...), K>{c}, std::get<P>(providers)...)};
   }
+
   _Injector<P...> build() { return {providers}; }
   std::tuple<P...> providers;
 };
